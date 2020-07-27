b2c5bc7de01c13cdc40c296d606ae666f981c0f3
Jordan Gregson <49405277+jordangregson@users.noreply.github.com>
2020-07-27 11:55:38 -0400

    associating data to specific state
    Attempting to associate data to a specific state in the US

--- state.js
+++ state.js
@@ -1,41 +1,36 @@
 const states_url = "https://covidtracking.com/api/states";
 
-//Finding the current location as longitude and latitude
-navigator.geolocation.getCurrentPosition(position => {
-    console.log(position.coords.latitude);
-    console.log(position.coords.longitude);
-
-
-    //Using the Google Maps API to figure out the location
-    const KEY = "AIzaSyDw5TFcEcRJw3Lh9Kc8U9vBfi21ZquZkpc";
-    const LAT = position.coords.latitude;
-    const LON = position.coords.longitude;
-    let url = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${LAT},${LON}&key=${KEY}`;
-
-    fetch(url)
-        .then(response => response.json())
-        .then(data => {
-            console.log(data);
-            let parts = data.results[0].address_components;
-            parts.forEach(part => {
-                if (part.types.includes("administrative_area_level_1")) {
-                    document.body.insertAdjacentHTML(
-                        'beforeend',
-                        `<p>State: ${part.short_name}</p>`)
-                }
-            });
-        })
-        .catch(err => console.warn(err.message));
-});
-
-
 
 
 async function getStateData() {
     const response = await fetch(states_url);
     const data = await response.json();
 
-
+    //Finding the current location as longitude and latitude
+    navigator.geolocation.getCurrentPosition(position => {
+    
+        //Using the Google Maps API to figure out the location 
+        const KEY = "AIzaSyDw5TFcEcRJw3Lh9Kc8U9vBfi21ZquZkpc";
+        const LAT = position.coords.latitude;
+        const LON = position.coords.longitude;
+        let url = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${LAT},${LON}&key=${KEY}`;
+    
+        fetch(url)
+            .then(response => response.json())
+            .then(loc => {
+                console.log(loc);
+                let parts = loc.results[0].address_components;
+                parts.forEach(part => {
+                    if (part.short_name.includes("PA")) {
+                        let test = document.getElementById("test");
+                        test.innerHTML = PA.negative;
+                            
+                    }
+                })
+                
+            })
+            .catch(err => console.warn(err.message));
+    });
 
 
 
